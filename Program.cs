c4f5575f7ba9ac6ecae7acc2db356f96dbfcd3db
Epulvis <zuhrisaifuddin405@gmail.com>
2025-03-16 12:51:24 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -2,7 +2,29 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        //Jika Menggunakan String
+        string nim = "103022300057";
+        DataGeneric<string> dataGeneric = new DataGeneric<string>(nim);
+        dataGeneric.PrintData();
+
+        //Jika Menggunakan Long
+        long angka = 103022300057;
+        DataGeneric<long> dataInt = new DataGeneric<long>(angka);
+        dataInt.PrintData();
+
+    }
+}
+
+class DataGeneric<T>
+{
+    private T Data { get; set; }
+    public DataGeneric(T data)
+    {
+        Data = data;
+    }
+    public void PrintData()
+    {
+        Console.WriteLine($"Data yang tersimpan adalah: {Data}");
     }
 }
 
